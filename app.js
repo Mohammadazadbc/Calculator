a2bab59a57945b5303664afa961a7d86cdaa5c51
azad <mohammadazad.bc~gmail.com>
2021-09-27 16:39:47 +0200

    final

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-let inputValue = document.getElementById('input-value');
-const numbers = document.querySelectorAll('.numbers')
-const ac = document.getElementById('ac')
-let egal = document.getElementById('egal')
-numbers.forEach((number)=>{
-    number.addEventListener('click',(e)=>{
-        inputValue.value += e.target.innerText;
-    
-    })
+const turnOff = document.getElementById('turn-off')
+
+
+
+function data(val){
+    calc.display.value += val;
+}
+
+function ac(val){
+    calc.display.value =""
+}
+function c(val){
+    calc.display.value = calc.display.value.slice(0, -1)
+}
+
+function egal(){
+    calc.display.value = eval(calc.display.value);
+}
+
+turnOff.addEventListener('click', ()=>{
+    window.close();
 })
-
-ac.addEventListener('click',()=>{
-    inputValue.value = "";
-})
-
-
-
-
